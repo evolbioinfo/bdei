ace3941ef6f747f6931c777cf043455235f02cdf
Anna Zhukova <zhutchok@gmail.com>
2024-08-05 23:47:50 +0200

    v0.9: fixed dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 
 # ensure local python is preferred over distribution python
 ENV PATH /usr/local/bin:$PATH
@@ -12,10 +12,10 @@ ARG DEBIAN_FRONTEND=noninteractive
 
 # dependencies
 RUN apt update --fix-missing 
-RUN apt install -y g++-10 libnlopt-cxx-dev bc python3.9 python3-pip python3-setuptools python3-distutils
+RUN apt install -y g++ libnlopt-cxx-dev bc python3 python3-pip python3-setuptools python3-distutils
 
 # Install pybdei
-RUN cd /usr/local/ && pip3 install --no-cache-dir numpy && pip3 install --no-cache-dir pybdei==0.8 && pip3 install --no-cache-dir pandas
+RUN cd /usr/local/ && pip3 install --no-cache-dir numpy && pip3 install --no-cache-dir pybdei==0.9 && pip3 install --no-cache-dir pandas
 
 # File Author / Maintainer
 MAINTAINER Anna Zhukova <anna.zhukova@pasteur.fr>
--- src/setup.py
+++ src/setup.py
@@ -5,8 +5,8 @@ import os
 from setuptools import setup, Extension, find_packages
 import numpy as np
 
-os.environ["CC"] = "g++-10"
-os.environ["CXX"] = "g++-10"
+os.environ["CC"] = "g++"
+os.environ["CXX"] = "g++"
 
 # the C++ extension module
 bdei_module = Extension('_pybdei',
@@ -30,13 +30,15 @@ setup(
         'Programming Language :: Python :: 3 :: Only'
     ],
     packages=find_packages(),
-    install_requires=['numpy>=1.24.0', 'ete3>=3.1.3', 'six>=1.16.0', 'scipy>=1.11.1', 'treesimulator>=0.1.22'],
-    setup_requires=['numpy>=1.24.0'],
+    install_requires=['numpy>=1.24.0', 'ete3>=3.1.3', 'six>=1.16.0', 'scipy>=1.11.1', 'treesimulator>=0.1.22',
+                      'nlopt>=2.6.1'],
+    setup_requires=['numpy>=1.24.0', 'nlopt>=2.6.1'],
     include_package_data=True,
     package_data={'pybdei': ['*.hpp'],
                   'bdei': ['*.hpp']},
-    version='0.8',
-    description='Fast and accurate epidemiological parameter estimation from phylogenetic trees with the Birth-Death Exposed-Infectious (BDEI) model.',
+    version='0.9',
+    description='Fast and accurate epidemiological parameter estimation from phylogenetic trees '
+                'with the Birth-Death Exposed-Infectious (BDEI) model.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Frédéric Heicht',
